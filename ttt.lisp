408d5f241612889be2b2cce6542f3374e53c538a
Michael Malis <michaelmalis2@gmail.com>
2015-06-25 16:51:43 -0400

    Refactored the rows and columns.

--- ttt.lisp
+++ ttt.lisp
@@ -2,10 +2,23 @@
 (in-package :server)
 (syntax:use-syntax :clamp)
 
+(defparameter dims* 5 "The side length of the tic-tac-toe board.")
+
 (deftem (tic-tac-toe (:conc-name nil) (:include game))
-  (need 2)
+  (need 2) ; The number of players needed to play a game.
   current
-  (board (make-array '(3 3) :initial-element nil)))
+  (board (make-array (list dims* dims*) :initial-element nil)))
+
+(defmethod print-object ((game tic-tac-toe) stream)
+  (let *standard-output* stream
+    (up r 0 dims*
+      (up c 0 dims*
+        (prf "~:[ ~;~:*~A~]" game!board.r.c)
+        (unless (is c (dec dims*))
+          (pr "|")))
+      (prn)
+      (unless (is r (dec dims*))
+        (prn (make-string (+ dims* dims* -1) :initial-element #\-))))))
 
 (defmethod start-game ((game tic-tac-toe))
   "Initialize the actual game."
@@ -20,25 +33,45 @@
   (send-ai game!current "0~%")
   (send-ai game!next    "1~%"))
 
+(defcont play-ttt-turn (game) (socket)
+  "Performs a turn for the current player."
+  (declare (ignore socket))
+  (mvb (r c) (read-input game game!current)
+    (= game!board.r.c (if (is game!current game!players!car) 'x 'o))
+    (send-hu game!players "~A" game ())
+    (aif (winner game)
+      (do (announce-winner game)
+          (disconnect game))
+      (do ;; We only want to send the move if the game is still going
+          ;; on. If the game is over, there is no reason to send the
+          ;; opponents move.
+          (send-ai game!next "~A ~A~%" r c)
+          (= game!current (next game))
+          (send-hu game!current "Your turn.~%" ())
+          (push game!current!socket sockets*)
+          (= (temp-cont game!current!socket) (play-ttt-turn game))))))
+
 (def winner (game)
   "Is there a winner of this game? If so return the piece of that player."
-  (or (iter (for r from 0 below 3)
-            (thereis (iter (for c from 0 below 3)
+  ;; By using and in the way below, thereturn value will be the
+  ;; winning player.
+  (or (iter (for r from 0 below dims*)
+            (thereis (iter (for c from 0 below dims*)
                            (always (and (is game!board.r.c game!board.r.0) game!board.r.0)))))
-      (iter (for c from 0 below 3)
-            (thereis (iter (for r from 0 below 3)
+      (iter (for c from 0 below dims*)
+            (thereis (iter (for r from 0 below dims*)
                            (always (and (is game!board.r.c game!board.0.c) game!board.0.c)))))
-      (and game!board.0.0
-           (is game!board.0.0 game!board.1.1)
-           (is game!board.0.0 game!board.2.2)
+      (and (iter (for i from 0 below dims*)
+                 (always (and (is game!board.i.i game!board.0.0))))
            game!board.0.0)
-      (and game!board.0.2
-           (is game!board.0.2 game!board.1.1)
-           (is game!board.0.2 game!board.2.0)
-           game!board.0.2)
+      (and (iter (for r from 0 below dims*)
+                 (for c from (dec dims*) downto 0)
+                 (always (and (is game!board.r.c (get game!board.0 (dec dims*)))
+                              game!board.r.c)))
+           (get game!board.0 (dec dims*)))
       (and (iter out
-                 (for r from 0 below 3)
-                 (iter (for c from 0 below 3)
+                 (for r from 0 below dims*)
+                 (iter (for c from 0 below dims*)
                        (in out (always game!board.r.c))))
            'tie)))
 
@@ -64,44 +97,15 @@
                :format-control "'~A' is malformed input."
                :format-arguments (list )))
       (let (r c) (map #'parse-integer strings)
-        (unless (<= 0 r 2)
+        (unless (<= 0 r (dec dims*))
           (error 'invalid-move
                  :format-control "The row index ~A is illegal."
                  :format-arguments (list r)))
-        (unless (<= 0 c 2)
+        (unless (<= 0 c (dec dims*))
           (error 'invalid-move
                  :format-control "The column index ~A is illegal."
                  :format-arguments (list r)))
         (when game!board.r.c
           (error 'invalid-move :format-control "The square is already taken."))
         (values r c)))))
-
-(defmethod print-object ((game tic-tac-toe) stream)
-  (let *standard-output* stream
-    (up r 0 3
-      (up c 0 3
-        (prf "~:[ ~;~:*~A~]" game!board.r.c)
-        (unless (is c 2)
-          (pr "|")))
-      (prn)
-      (unless (is r 2)
-        (prn "-----")))))
-
-(defcont play-ttt-turn (game) (socket)
-  "Performs a turn for the current player."
-  (declare (ignore socket))
-  (mvb (r c) (read-input game game!current)
-    (= game!board.r.c (if (is game!current game!players!car) 'x 'o))
-    (aif (winner game)
-      (do (announce-winner game)
-          (disconnect game))
-      (do (send-hu game!players "~A" game ())
-          ;; We only want to send the move if the game is still going
-          ;; on. If the game is over, there is no reason to send the
-          ;; opponents move.
-          (send-ai game!next "~A ~A~%" r c)
-          (= game!current (next game))
-          (send-hu game!current "Your turn.~%" ())
-          (push game!current!socket sockets*)
-          (= (temp-cont game!current!socket) (play-ttt-turn game))))))
 
