522882ecee602f1bd56793999032a9cda30dbea8
Michael Malis <michaelmalis2@gmail.com>
2015-06-28 15:22:30 -0400

    Added multiple ports to connect to when starting the server.
    Now when you start the server, you specify two ports, one for humans
    to connect to and the other for AI to connect to.

--- server.lisp
+++ server.lisp
@@ -26,44 +26,29 @@
 (defparameter disconnected-code* -1 "The code for when a player disconnects.")
 (defparameter unknown-error-code* -2 "The code for when an unknown error occurs.")
 
-;; (def start-server (game-type ports &rest args)
-;;   "Start a server playing a game of type GAME"
-;;   (unwind-protect (do (each port (mklist ports)
-;;                         (with (game (apply #'inst game-type args)
-;;                                     listener (socket-listen *wildcard-host* port :reuse-address t))
-;;                           (push listener sockets*)
-;;                           (= conts*.listener (add-player game))))
-;;                       (listening-loop))
-;;     (mapc #'safely-close sockets*)
-;;     (= sockets* '())))
-
-;; (def start-server (game-type hport aport &rest args)
-;;   "Start a server playing a game of type GAME. This opens two
-;;     sockets. One for aiplayers and one for human players."
-;;   (unwind-protect (do (let game (apply #'inst game-type args)
-;;                         (let listener (socket-listen *wildcard-host* hport :reuse-address t)
-;;                           (push listener sockets*)
-;;                           (push listener game->sockets*.game)
-;;                           (= (cont listener) (add-player game 'human)))
-;;                         (let listener (socket-listen *wildcard-host* aport :reuse-address t)
-;;                           (push listener sockets*)
-;;                           (push listener game->sockets*.game)
-;;                           (= (cont listener) (add-player game 'ai))))
-;;                       (listening-loop))
-;;     (mapc [disconnect _ t] (keys game->sockets*))))
-
-(def start-server (game-type port &rest args)
+(def start-server (game-type hport aport &rest args)
   "This version of start-server opens a single server which connects
    every two to a game of ttt. There is no limit on the number of
-   players."
-  (let listener (socket-listen *wildcard-host* port :reuse-address t)
-    (unwind-protect (do (push listener sockets*)
-                        ;(push listener game->sockets*.current-game*)
-                        (= (cont listener) (add-player (apply #'inst game-type args) 'human))
-                        (listening-loop))
-      (socket-close listener)
-      (= sockets* (rem listener sockets*))
-      (rem-cont listener)
+   players. Human players connect through HPORT and AI players connect
+   through APORT."
+  (with (hs (socket-listen *wildcard-host* hport :reuse-address t)
+         as (socket-listen *wildcard-host* aport :reuse-address t))
+    (unwind-protect (do (push hs sockets*)
+                        (push as sockets*)
+                        ;; Using a zero dimensional array as a pointer
+                        ;; so that both add-player closures always
+                        ;; refer to the same game.
+                        (let arr (make-array '())
+                          (= arr!aref (apply #'inst game-type args))
+                          (= (cont hs) (add-player arr 'human))
+                          (= (cont as) (add-player arr 'ai))
+                          (listening-loop)))
+      (socket-close hs)
+      (socket-close as)
+      (= sockets* (rem hs sockets*))
+      (= sockets* (rem as sockets*))
+      (rem-cont hs)
+      (rem-cont as)
       (each game (copy-list (keys game->sockets*))
         (send-hu game!players "An error occured~%.")
         (send-ai game!players "~A~%" unknown-error-code*)
@@ -96,6 +81,7 @@
                     :report "Disconnect the current game."
                     (send-hu socket->game*.socket!players "An unknown error occured.~%")
                     (send-ai socket->game*.socket!players "~A~%" unknown-error-code*)
+                    (disconnect socket->game*.socket)
                     (return)))
               :else
                 (restart-case (error "No continuation for socket ~A." socket)
@@ -121,22 +107,18 @@
              :if-does-not-exist :create)
       (mapc #'pr (reverse game!game-log)))))
 
-(defcont add-player (game type) (listener)
+(defcont add-player (arr type) (listener)
   "Wait for all of the players to connect."
   (withs (socket (socket-accept listener)
-          player (inst type :socket socket))
+          player (inst type :socket socket)
+          game arr!aref)
     (push socket game->sockets*.game)
     (push socket sockets*)
     (= socket->game*.socket game)
     (push player game!players)
     (when (is game!players!len game!need)
-      ;; (let listeners (set-difference game->sockets*.game (map #'socket game!players))
-      ;;   (= game->sockets*.game (set-difference game->sockets*.game listeners))
-      ;;   (= sockets* (set-difference sockets* listeners))
-      ;;   (mapc #'socket-close listeners))
-
       (start-game game)
       ;; Have the continuation add players to a new game instead of
       ;; the current one. It is safe to use type-of as it will always
       ;; return the class-name of a class with a proper name.
-      (= game (inst (type-of game))))))
+      (= arr!aref (inst (type-of game))))))
